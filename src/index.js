7eed72c37811be49c30335d7e00e5cd1ee256c87
Vishkash <vishkash.vishnu@gmail.com>
2022-08-12 15:08:03 +0530

    Enable all CORS requests

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const userRouter = require("./routers/user-router");
 const employeeRouter = require("./routers/employee-router");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 app.use(employeeRouter);
